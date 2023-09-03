8c13e61989f60c122a6f4275b0544b15346729e6
twg0 <fruitsjh@gmail.com>
2023-09-03 13:09:10 +0900

    Initial Project Commit

---
+++ src/main/java/com/example/redisexercise/RedisExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.redisexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisExerciseApplication.class, args);
+	}
+
+}
